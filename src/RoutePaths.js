4543ed8c2bee8e92610df6b0c3ad8e415fe4dc18
Shlok-Zanwar <Shlok-Zanwar@users.noreply.github.com>
2021-01-25 08:56:32 +0530

    Added multipage support for github pages

--- src/RoutePaths.js
+++ src/RoutePaths.js
@@ -1,16 +1,25 @@
-import React from 'react'
-import {BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React, { useEffect } from 'react'
+import {BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import BlogTemplate from './BlogTemplates/BlogTemplate';
 import Home from './HomeComponents/Home';
 import MyNavbar from "./MyNavbar";
 
 function RoutePaths() {
+    // let location = useLocation();
+    var forUseEffect;
+    useEffect(() => {
+        localStorage.removeItem("redirectTo");
+        // alert(window.location.pathname)
+    }, [forUseEffect])
+
     return (
         <Router>
             <MyNavbar />
 
             <Switch>
-            
+                {/* <Route exact path="/">
+                    
+                </Route> */}
                 <Route path="/blogs/toxicbot">
                     <BlogTemplate blogData={require("./BlogJsons/ToxicbotBlog.json")} />
                 </Route>
@@ -21,6 +30,11 @@ function RoutePaths() {
                     {/* <TodoApp /> */}
                 </Route>
                 <Route path="/">
+                    {
+                        localStorage.getItem('redirectTo') ? <Redirect to={localStorage.getItem('redirectTo')} /> 
+                        : null
+                        
+                    }
                     <Home />
                 </Route>
 
